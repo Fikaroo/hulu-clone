6b4e51705af8b4962e9983f7f3f99ce66c37f198
Fuad <fuadbayramv@gmail.com>
2022-08-13 19:41:55 +0300

    skeleton loading some updates

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
   const router = useRouter();
   return (
     <nav className="relative">
-      <div className="flex px-10 sm:px-20 2xl:text-2xl lg:text-xl text-2xl whitespace-nowrap space-x-10 overflow-x-scroll overflow-y-hidden scrollbar-hide">
+      <div className="flex px-10 sm:px-20 2xl:text-2xl lg:text-xl text-xl whitespace-nowrap space-x-10 overflow-x-scroll overflow-y-hidden scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -20,12 +20,12 @@ const Thumbnail = forwardRef(({ result }, ref) => {
   return (
     <div
       ref={ref}
-      className="p-2 group cursor-pointer transition-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+      className="p-2 group cursor-pointer rounded-md transition-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
     >
       <div className=" bg-slate-700 rounded-md">
         <div className={!data && "animate-pulse rounded-md"}>
           <Image
-            className={`${bgLoading}`}
+            className={`${bgLoading} rounded-t-md`}
             src={
               `${IMAGE_BASE_URL}${data.backdrop_path || data.poster_path}` ||
               `${IMAGE_BASE_URL}${data.poster_path}`
@@ -36,12 +36,22 @@ const Thumbnail = forwardRef(({ result }, ref) => {
             height={1080}
           />
           <div className="p-2">
-            <p className={`${bgLoading} h-6 truncate rounded-md max-w-md`}>
+            <p
+              className={
+                !data
+                  ? `${bgLoading} h-8 rounded-md`
+                  : `${bgLoading} truncate rounded-md max-w-md`
+              }
+            >
               {data.overview}
             </p>
 
             <h2
-              className={`${bgLoading} h-8 mt-1 text-2xl rounded-md text-white transition-all duration-100 ease-in-out group-hover:font-bold`}
+              className={
+                !data
+                  ? `${bgLoading} h-8 mt-1 rounded-md `
+                  : `${bgLoading} mt-1 rounded-md text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold`
+              }
             >
               {data.title || data.original_name}
             </h2>
